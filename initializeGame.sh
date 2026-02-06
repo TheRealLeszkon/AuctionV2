0ce5c428936e875d78e366c99f75f7bc2c72b10d
TheRealLeszkon <jmkevin2006@gmail.com>
2026-02-06 13:23:33 +0530

    Tiny fix #1

--- initializeGame.sh
+++ initializeGame.sh
@@ -20,7 +20,7 @@ curl --location 'localhost:6769/game' \
     "bowlersPerTeam":4,
     "allRounderPerTeam":2,
     "wicketKeeperPerTeam":1,
-    "substitutesPerTeam":15,
+    "substitutesPerTeam":3,
     "unCappedPerTeam":1,
     "legendsPerTeam": 1,
     "specialPlayersPerTeam": 2,
